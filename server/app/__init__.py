5a42e5f3e6eecfa1aaf76ba73d57a18b43fbeecb
psyche <psychesworld@gmail.com>
2017-10-08 22:50:54 +0800

    Enables CORS support

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 
 from config import config
@@ -8,6 +9,7 @@ db = SQLAlchemy()
 
 def create_app(config_name):
     app = Flask(__name__)
+    CORS(app)
     app.config.from_object(config[config_name])
     db.init_app(app)
 
